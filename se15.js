afe56d1dfcf342863f428f4cf5aebbf26e051b2c
shahryar.bhrz <shahryar.bhrz@gmail.com>
2023-10-27 23:12:35 +0330

    start js

---
+++ se15.js
@@ -0,0 +1,23 @@
+// const shabah = alert('hi')
+
+
+// console.log(number)
+// const numEight = document.getElementById('#hasht')
+// console.log(numEight)
+
+// const onClick = () => {
+//     console.log(number.innerText)
+// }
+
+const numberSeven = document.getElementsByClassName('left-number')[0];
+
+let current =  0
+const mathLine = document.getElementsByTagName("h2")[0];
+const newMathLine = () => {
+    mathLine.innerText = current
+}
+
+newMathLine()
+
+
+
